ce9cfdbbfa356530f8901ad91c3487e06242185f
João Neto <joaodeveloper29@gmail.com>
2025-10-01 19:35:10 -0300

    :sparkles: feature(:frontend): adding main page

--- portais-app/app/page.tsx
+++ portais-app/app/page.tsx
@@ -1,5 +1,46 @@
-import Header from "@/components/shared/header";
+import CardInfo from "@/components/cardInfo";
+import CardOffer from "@/components/cardOffer";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <CardInfo />
+      <Box
+        sx={{
+          opacity: 1,
+          gap: "24px",
+          paddingRight: "88px",
+          paddingBottom: "56px",
+          paddingLeft: "88px",
+        }}
+      >
+        <Box
+          sx={{
+            opacity: 1,
+            paddingTop: "32px",
+            paddingBottom: "16px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "400",
+              fontSize: "14px",
+              leadingTrim: "NONE",
+              lineHeight: "133%",
+              letterSpacing: "0%",
+            }}
+          >
+            2 opções encontradas
+          </Typography>
+        </Box>
+        <CardOffer />
+      </Box>
+      <Footer />
+    </>
+  );
 }
