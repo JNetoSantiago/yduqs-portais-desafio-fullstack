a73d5744e0946c0d6c3f7b234a21c2352c2e31f2
João Neto <joaodeveloper29@gmail.com>
2025-10-01 19:34:53 -0300

    :sparkles: feature(:frontend): adding card offer and card info

---
+++ portais-app/components/cardInfo.tsx
@@ -0,0 +1,46 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+export default function CardInfo() {
+  return (
+    <Card
+      sx={{
+        width: "100%",
+        height: 150,
+        opacity: 1,
+        p: "40px 88px",
+        gap: 2,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        backgroundColor: "primary.main",
+        borderRadius: 0,
+        color: "#FFFFFF",
+      }}
+      elevation={0}
+    >
+      <CardContent sx={{ p: 0 }}>
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: 500,
+            fontSize: 32,
+            lineHeight: "120%",
+            letterSpacing: 0,
+          }}
+        >
+          Vamos começar, escolha as opções do seu curso
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: 400,
+            fontSize: 16,
+            lineHeight: "150%",
+            letterSpacing: 0,
+          }}
+        >
+          Selecione o curso que deseja e clique em avançar
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ portais-app/components/cardOffer.tsx
@@ -0,0 +1,142 @@
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+
+export default function CardOffer() {
+  return (
+    <Card
+      sx={{
+        borderColor: "primary.main",
+        borderWidth: "1px",
+        borderStyle: "solid",
+        width: "381px",
+      }}
+    >
+      <CardContent sx={{ p: 0 }}>
+        <Box
+          sx={{
+            width: "381px",
+            height: "38px",
+            opacity: 1,
+            gap: "8px",
+            paddingTop: "8px",
+            paddingRight: "24px",
+            paddingBottom: "8px",
+            paddingLeft: "24px",
+            borderBottomWidth: "1px",
+            backgroundColor: "primary.dark",
+            color: "#FFFFFF",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: 400,
+              fontSize: 16,
+              lineHeight: "150%",
+              letterSpacing: 0,
+            }}
+          >
+            Presencial | Manhã
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: "381px",
+            height: "209px",
+            opacity: 1,
+            gap: "24px",
+            padding: "24px",
+            backgroundColor: "primary.main",
+            color: "#FFFFFF",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "500px",
+              fontStyle: "Medium",
+              fontSize: "16px",
+              leadingTrim: "NONE",
+              lineHeight: "114.99999999999999%",
+              letterSpacing: "0%",
+            }}
+          >
+            De R$ 4.752,00 por até
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Typography
+              sx={{
+                opacity: 1,
+                gap: "8px",
+                paddingRight: "2px",
+                paddingBottom: "2px",
+              }}
+            >
+              18x
+            </Typography>
+            <Typography
+              sx={{
+                fontWeight: "600",
+                fontStyle: "SemiBold",
+                fontSize: "40px",
+                leadingTrim: "NONE",
+                lineHeight: "114.99999999999999%",
+                letterSpacing: "0%",
+              }}
+            >
+              R$ 169,95
+            </Typography>
+          </Box>
+          <Typography
+            sx={{
+              opacity: 1,
+              gap: "8px",
+              paddingRight: "2px",
+              paddingBottom: "2px",
+            }}
+          >
+            à vista R$ 2.613,60
+          </Typography>
+          <Button variant="contained" color="secondary" fullWidth>
+            Avançar
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            opacity: 1,
+            gap: "24px",
+            padding: "24px",
+            borderBottomRightRadius: "8px",
+            borderBottomLeftRadius: "8px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "500",
+              fontStyle: "Medium",
+              fontSize: "14px",
+              leadingTrim: "NONE",
+              lineHeight: "135%",
+              letterSpacing: "0%",
+            }}
+          >
+            CAMPINAS - VILA INDUSTRIAL
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: "400",
+              fontStyle: "Regular",
+              fontSize: "14px",
+              leadingTrim: "NONE",
+              lineHeight: "114.99999999999999%",
+              letterSpacing: "0%",
+            }}
+          >
+            RUA DR. SALES DE OLIVEIRA, Nº 1661 - VILA INDUSTRIAL - CAMP...
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
